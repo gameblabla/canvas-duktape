311a9ff6a35537f796b43a2b86bed6591080bc6c
gameblabla <gameblabla@protonmail.com>
2023-02-12 11:38:39 +0100

    We are going backwards lol

--- canvas.js
+++ canvas.js
@@ -1,5 +1,15 @@
 var first_layer = document.getElementById('canvas');
-var MyC = first_layer.getContext('2d');
+if (!first_layer) {
+  throw new Error('Canvas element not found');
+}
+console.log(first_layer.id);
+if (typeof first_layer.id !== 'string') {
+  throw new Error('Canvas element has invalid id');
+}
+if (typeof first_layer.getContext !== 'function') {
+  throw new Error('getContext method is not available');
+}
+//var MyC = first_layer.getContext('2d');
 /*var img;
 img = new Image();
 img.src = 'image.bmp';
